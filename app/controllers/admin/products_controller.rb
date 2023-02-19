aacc3cd0d49856af336253355a1b3d2733be7dbe
AiriNarita <airiswim.kitty@gmail.com>
2023-02-19 19:20:29 +0900

    feat: product/new-edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,35 @@
 class Admin::ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+    @genre = Genre.all
+    @btn = "新規登録"
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product) 
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.new
+    @genre = Genre.all
+    @btn = "変更を保存"
+  end
+
+  def update
+  end
+
+  
 end
